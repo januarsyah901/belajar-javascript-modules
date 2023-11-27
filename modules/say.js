df26282bd234ad4c0f2d11d9c846c1892cf8402d
januar <januar@pop-os.localdomain>
2023-11-27 19:54:44 +0700

    javascript module

---
+++ modules/say.js
@@ -0,0 +1,10 @@
+function info(params) {
+  console.info(params);
+}
+export function sayHello(name) {
+  info(`hello ${name}`);
+}
+export function sayGoodBye(name) {
+  info(`bye bye ${name}`);
+}
+export const person = "akbar";
